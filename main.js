b9c09e4679b18ed10d16c13c91424db21fd4a9dd
yoochan0108 <yoochan0108@naver.com>
2023-09-21 09:32:43 +0900

    else문이 필요 없는 구문은 &&연산자로 처리

--- main.js
+++ main.js
@@ -40,15 +40,15 @@ const sec = now.getSeconds();
 console.log(setNumbers(hours));
 
 function setNumbers(num) {
-	let result = 0;
 	/*
-	if (num < 10) result = '0' + num;
-	else result = num;
+	if (num < 10) num = '0' + num;
 	*/
 	//조건식 ? true일 때 실행할 구문 : false일 때 실행할 구문
 	//삼항연산자 구문안에는 대입연산자가 들어올 수 없기 때문에 괄호로 묶어서 표현식으로 변경
-	num < 10 ? (result = '0' + num) : (result = num);
-	return result;
+	//특정값이 else일때 굳이 실행할 필요가 없다면 &&연산자 활용
+	//num < 10 ? (result = '0' + num) : (result = num);
+	num < 10 && (num = '0' + num);
+	return num;
 }
 /*
 let newHr = 0;
