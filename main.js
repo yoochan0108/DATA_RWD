81f37ef3c6538219aef5cca4eec7f8af8cc948f2
yoochan0108 <yoochan0108@naver.com>
2023-09-21 11:18:35 +0900

    시간에 따라서 클래스 전환

--- main.js
+++ main.js
@@ -5,6 +5,16 @@
 const [hr, m, s] = document.querySelectorAll('h1 span');
 setInterval(setTime, 1000);
 
+//함수추가
+//원하는 시간마다 body에 morning, afternoon, evening, night
+// if(hours >=6 && hours< 11) document.body.classList.add('morning')
+//주의점 밤은 특정이상 혹은 일정시간 이하 (&&가 아닌 ||로 표현가능)
+function changeClass(hr) {
+	if (hr >= 6 && hr < 12) document.body.classList.add('morning');
+	if (hr >= 12 && hr < 16) document.body.classList.add('afternoon');
+	if (hr >= 16 && hr < 20) document.body.classList.add('evening');
+	if (hr >= 20 || hr < 6) document.body.classList.add('night');
+}
 function setTime() {
 	const now = new Date();
 	const hours = now.getHours();
@@ -13,6 +23,7 @@ function setTime() {
 	hr.innerText = setNumbers(setHours(hours));
 	m.innerText = setNumbers(min);
 	s.innerText = setNumbers(sec);
+	changeClass(hours);
 }
 
 /*
